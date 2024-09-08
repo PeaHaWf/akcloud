c6b9ddc647e4ada9a71c45c2f6fc2243b7e22c0c
Akejyo <987383393@qq.com>
2024-09-08 12:28:43 +0800

    Add Menu to AddFile

--- akcloud/src/renderer/src/components/AddFile/index.tsx
+++ akcloud/src/renderer/src/components/AddFile/index.tsx
@@ -1,18 +1,34 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, Menu, MenuItem } from '@mui/material'
 import Add from '@mui/icons-material/Add'
+import { useState } from 'react'
 const AddFile = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Button
-      variant="contained"
-      startIcon={<Add />}
-      size="small"
-      sx={{
-        padding: '0px 5px !important',
-        height: '40px !important'
-      }}
-    >
-      添加文件
-    </Button>
+    <>
+      <Button
+        variant="contained"
+        startIcon={<Add />}
+        size="small"
+        sx={{
+          padding: '0px 5px !important',
+          height: '40px !important'
+        }}
+        onClick={handleClick}
+      >
+        添加文件
+      </Button>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>上传文件</MenuItem>
+        <MenuItem onClick={handleClose}>上传文件夹</MenuItem>
+      </Menu>
+    </>
   )
 }
 export default AddFile
