5ebe30aa205f804571846ba1acd011eb59aba9e9
Akejyo <987383393@qq.com>
2025-01-04 21:35:03 +0800

    update

--- akcloud-backend/methods/pack_file.h
+++ akcloud-backend/methods/pack_file.h
@@ -4,10 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
 
 class PackFile {
 public:
-    static bool packFile(const std::string &sourcePath, const std::string &backupPath);
+    static bool packFile(const std::string &filePath, const std::string packFilePath);
+    static bool unpackFile(const std::string &packFilePath, const std::string &unpackFilePath);
 };
 
-#endif
+#endif // PACK_FILE_H
