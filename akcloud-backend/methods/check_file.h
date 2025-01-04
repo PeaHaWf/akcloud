3d25ab5038b71147a880288c141157503c4c1536
Akejyo <987383393@qq.com>
2025-01-04 21:33:59 +0800

    add check_crc

---
+++ akcloud-backend/methods/check_file.h
@@ -0,0 +1,19 @@
+#ifndef CHECK_FILE_H
+#define CHECK_FILE_H
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+class CheckFile {
+public:
+    static std::uint32_t generateCRC(const std::string &filePath);
+    static bool checkFile(const std::string &filePath);
+
+private:
+    static std::uint32_t crc32(const void *data, std::size_t length);
+    static std::uint32_t crc32_table[256];
+    static void generate_crc32_table();
+};
+
+#endif // CHECK_FILE_H
