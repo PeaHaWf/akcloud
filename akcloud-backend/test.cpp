140a2d8b5badaef4324eb53f7023fe4110f6c046
peahawf <1161428420@qq.com>
2025-01-06 06:41:30 +0800

    test

--- akcloud-backend/test.cpp
+++ akcloud-backend/test.cpp
@@ -5,12 +5,12 @@
 #include "methods/decomposer.h"
 int main() {
     //压缩指定路径下文件到指定输出文件
-    Composer composer("D:\\mav\\testtxt.txt", "out");
+    Composer composer("D:\\mav\\testtxt.txt", "D:\\mav\\compressed");
      //composer.compress_lz77();
 
     composer.startCompose();
     //读取待解压文件,设置输出文件名（后缀不需要）
-    DeComposer decomposer("out.hf", "output");
+    DeComposer decomposer("D:\\mav\\compressed.hf", "D:\\mav\\decompressed");
     
     decomposer.decomposeAlgorithmSelector();
 
